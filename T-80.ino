e70ae10599257310bb3c874eba0f7ac91c8871f3
Matthias Kreier <43933271+kreier@users.noreply.github.com>
2019-03-06 21:31:13 +0700

    motor driver L298N test

---
+++ T-80.ino
@@ -0,0 +1,17 @@
+// remote robot control with bluetooth
+
+void setup() {
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+}
+
+void loop() {
+  for(int i = 9; i < 13; i++) {
+    digitalWrite(i, HIGH);
+    delay(1000);
+    digitalWrite(i, LOW);
+    delay(1000);
+  }
+}

44fb1cfb10a731675370fc135c8d41ffe454f25b
Matthias Kreier <43933271+kreier@users.noreply.github.com>
2019-03-08 01:23:01 +0700

    Add files via upload

---
+++ T-80bluetooth.ino
@@ -0,0 +1,46 @@
+// remote robot control with bluetooth
+#include <SoftwareSerial.h>
+
+SoftwareSerial BTSerial(2, 3);   // RX | TX
+
+char BTinput = '0';
+
+void setup() {
+  Serial.begin(9600);
+  BTSerial.begin(9600);  // HC-05 default speed in AT 38400
+  
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+}
+
+void loop() {
+  if (BTSerial.available()) 
+  {
+    BTinput = BTSerial.read();
+    Serial.write(BTinput);
+    if (BTinput == 'U') {
+      digitalWrite(9, HIGH);
+      digitalWrite(11, HIGH);
+    }
+    if (BTinput == 'D') {
+      digitalWrite(10, HIGH);
+      digitalWrite(12, HIGH);
+    }
+    if (BTinput == 'L') {
+      digitalWrite(9, HIGH);
+      digitalWrite(12, HIGH);
+    }
+    if (BTinput == 'R') {
+      digitalWrite(10, HIGH);
+      digitalWrite(11, HIGH);
+    }
+    if (BTinput == '0') {
+      digitalWrite(9, LOW);
+      digitalWrite(10, LOW);
+      digitalWrite(11, LOW);
+      digitalWrite(12, LOW);
+    }
+  }
+}
